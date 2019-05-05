3b16ef17ef21b479c75b9527573e47ba7a078938
Jinki Tanaka <jinkitana@hotmail.com>
2019-05-06 02:12:11 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+aws-billing-notification
+========================
+
+Description
+-----------
+ This applicaion nofify your aws cost with email every day on 0:00 AM (UTC).
+
+
+Quick install
+-------------
+1. You have set your account to allow IAM and federated users to access billing information on your root account with the following url.
+    * https://docs.aws.amazon.com/awsaccountbilling/latest/aboutv2/grantaccess.html
+2. You have to install and configure awscli as following url. 
+    * https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html
+    * The IAM user you use for deploying should be set AdminAccessPolicy.
+3. Pull the repository and change directory to `aws-billing-notification`
+    * `git clone https://github.com/j-tanaka/aws-billing-notification.git`
+4. Setup your email address for notifing the aws cost.
+    * `echo "email: [your email address]" > serverless_config.yml`
+    * for example, `echo "email: mail@example.net" > serverless_config.yml`
+5. Install the dependencies
+    * `npm install`
+6. Deploy aws-billing-notification to your aws account.
+    * `npm run deploy`
+7. Activate SNS subscription as you received email from AWS.
+8. Test aws-billing-notification
+    * `npm run prod`
+
+
+*  
+
+
---
+++ handler.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const costexplorer = new AWS.CostExplorer
+const dateformat = require('dateformat');
+const sns = new AWS.SNS;
+
+module.exports.publishBillingNotification = async (event) => {
+  
+  let startdate = new Date();
+  let enddate = new Date();
+  startdate.setDate(1);
+  enddate.setDate(1);
+  enddate.setMonth(enddate.getMonth()+1)
+  enddate.setDate(0);
+  
+  const params = {
+    Granularity: 'MONTHLY',
+    Metrics: [ 'UnblendedCost' ],
+    GroupBy: [{
+      Type: 'DIMENSION',
+      Key: 'SERVICE',
+    }],
+    TimePeriod:{
+      Start: dateformat(startdate, "yyyy-mm-dd"),
+      End: dateformat(enddate, "yyyy-mm-dd"),
+    }
+  };
+  
+  let data = {};
+  
+  try{
+    data = await costexplorer.getCostAndUsage(params).promise();
+    let text = "DAILY REPORT OF AWS COST ";
+    data.ResultsByTime.map(result => {
+      text += 'Start:' + result.TimePeriod.Start + '，End:' + result.TimePeriod.End + "\n";
+      result.Groups.map(group => {
+        group.Keys.map(key => text += key.padStart(50) + ': ');
+        text += '$' + group.Metrics.UnblendedCost.Amount + "\n";
+      });
+    })
+    const sns_param = {
+      TopicArn: process.env.SNS_TOPIC_ARN, 
+      Message: JSON.stringify(data, null, 2),      
+    }
+    console.log(text);
+    let result = await sns.publish(sns_param).promise();
+    
+    
+  }catch(err){
+    console.error(err,err.stack);
+    return {
+    statusCode: 500,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      output: err,
+    }, null, 2),
+  };
+  }
+  
+  
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      output: data,
+    }, null, 2),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
